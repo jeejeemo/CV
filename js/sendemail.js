5442e57fe21099cdc18b5c5819ef1a2690d5b9f2
jeanmo <jmbitoun@gmail.com>
2018-01-10 21:24:28 +0100

    Insert profil  pro

--- js/sendemail.js
+++ js/sendemail.js
@@ -27,7 +27,7 @@
                 if(validateForm()){
                  e.preventDefault();
                   $.ajax({type: "POST",
-                          url: "http://www.yourdomain.com/send-email.php",
+                          url: "http://cv.jeanmauricebitoun.fr/send-email.php",
                           data:$("#form1").serialize(),
                           success:function(result){
                           $("#successmsg").html(result);
